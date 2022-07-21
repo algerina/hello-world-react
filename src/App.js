1a4560cec16f8e68149e9944a3ce1759e7fe4159
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-07-21 13:03:56 +0100

    Create store

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import "./App.css";
+import ReactDOM from 'react-dom/client';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to React Router!</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
+    <div className='outer-container'>
+      <div className='inner-container'>
+        <Routes>
+          <Route path='/' element={<GreetingView />} />
+        </Routes>
+      </div>
     </div>
   );
 }
+
+export default App;
